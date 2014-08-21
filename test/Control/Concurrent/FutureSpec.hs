f7a626d182f187bee15d2b700b9bb590167195c5
Derek Williams <derek@fyrie.net>
2014-08-21 22:49:43 +0100

    test executeIO

--- test/Control/Concurrent/FutureSpec.hs
+++ test/Control/Concurrent/FutureSpec.hs
@@ -8,8 +8,15 @@ main = hspec spec
 
 spec :: Spec
 spec = do
+
   describe "future" $ do
+
     it "executes pure code" $ do
       foo <- execute "foo"
+      foo' <- await foo
+      foo' `shouldBe` "foo"
+
+    it "executes IO code" $ do
+      foo <- executeIO $ return "foo"
       foo' <- await foo
       foo' `shouldBe` "foo"
